08ef9d23fe7dce1b6443c4a01836ac036d000521
xiaobo wei <1362596047@qq.com>
2021-11-13 22:36:17 +0800

    enum

---
+++ base/src/main/java/com/pl1111w/base/finalclass/Bank.java
@@ -0,0 +1,36 @@
+package com.pl1111w.base.finalclass;
+
+/**
+ * @title: pl1111w
+ * @description: jdk 1.5之前定义枚举
+ * @author: Kris
+ * @date 2021/11/13 22:08
+ */
+public class Bank {
+
+    private final String bankName;
+    private final String bankCountry;
+
+    private Bank(String bankName, String bankCountry) {
+        this.bankCountry = bankCountry;
+        this.bankName = bankName;
+    }
+    private static final Bank ICBC = new Bank("ICBC","CHN");
+    private static final Bank HSBC = new Bank("HSBC","US");
+    private static final Bank CITI = new Bank("CITI","USA");
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public static void main(String[] args) {
+        Bank bank = Bank.CITI;
+        System.out.println(bank.getBankCountry());
+    }
+
+
+}
---
+++ base/src/main/java/com/pl1111w/base/finalclass/BankEnum.java
@@ -0,0 +1,44 @@
+package com.pl1111w.base.finalclass;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+/**
+ * @title: pl1111w
+ * @description: jdk 1.5之后定义枚举
+ * @author: Kris
+ * @date 2021/11/13 22:08
+ */
+public enum BankEnum {
+
+    ICBC("ICBC", "CHN"),
+    HSBC("HSBC", "US"),
+    CITI("CITI", "USA");
+
+    private final String bankName;
+    private final String bankCountry;
+
+    BankEnum(String bankName, String bankCountry) {
+        this.bankName = bankName;
+        this.bankCountry = bankCountry;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BankEnum.CITI);
+        System.out.println("===valueOf==");
+        BankEnum bank = BankEnum.valueOf("HSBC");
+        System.out.println(bank);
+        System.out.println("===values==");
+        BankEnum[] banks = BankEnum.values();
+        for (BankEnum bankEnum : banks) {
+            System.out.println(bankEnum);
+        }
+    }
+}
