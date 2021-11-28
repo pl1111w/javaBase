d064e6834e9f598b982ac3e1520b5bf79c1fa17a
xiaobo wei <1362596047@qq.com>
2021-11-28 21:28:18 +0800

    IO.File

---
+++ base/src/main/java/com/pl1111w/base/collectionTest/HashMapTest.java
@@ -0,0 +1,26 @@
+package com.pl1111w.base.collectionTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @title: pl1111w
+ * @description:
+ * @author: Kris
+ * @date 2021/11/24 14:28
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap();
+        map.put("1","one");
+        map.put("2","two");
+        map.put("3","three");
+        System.out.println(map);
+        System.out.println(map.keySet());
+        for(Map.Entry entry:map.entrySet()){
+            System.out.println(entry.getValue());
+        }
+
+    }
+}
---
+++ base/src/main/java/com/pl1111w/base/ioFile/FilesTest.java
@@ -0,0 +1,45 @@
+package com.pl1111w.base.ioFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @title: pl1111w
+ * @description:
+ * @author: Kris
+ * @date 2021/11/28 20:53
+ */
+public class FilesTest {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("hello.txt");
+        System.out.println(file.getName());
+        File file1 = new File("F:\\codes\\base","hello/Package");
+        System.out.println(file1.getPath());
+        File file2 = new File(file1,"hello.txt");
+        System.out.println(file2.getAbsolutePath());
+
+        if(file.exists()){
+            System.out.println("file is exist");
+        }else {
+            file.createNewFile();
+            System.out.println(file.getName());
+            System.out.println(file.length());
+            System.out.println(new Date(file.lastModified()));
+            System.out.println(file.canWrite());
+        }
+
+        boolean mkdir = file1.mkdir();
+        boolean mkdirs = file1.mkdirs();
+        if(!mkdir){
+            System.out.println("创建失败");
+        }
+        else if(mkdirs){
+            System.out.println("创建成功："+mkdirs);
+        }
+
+        System.out.println("父级目录："+ file2.getParent());
+
+    }
+}
